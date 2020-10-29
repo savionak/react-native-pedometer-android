d32657acf19e00a30045c877b5c2e264b8549dc7
Vladislav Savionok <savionak@gmail.com>
2020-10-29 12:40:24 +0300

    refactor: pedometer module

--- android/src/main/java/com/savionak/reactnative/pedometer/PedometerModule.java
+++ android/src/main/java/com/savionak/reactnative/pedometer/PedometerModule.java
@@ -1,11 +1,17 @@
 package com.savionak.reactnative.pedometer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 public class PedometerModule extends ReactContextBaseJavaModule {
+
+  private static final String MODULE_NAME = "Pedometer";
+
   private StepCounter mStepCounter;
   private ReactApplicationContext mReactContext;
 
@@ -14,47 +20,58 @@ public class PedometerModule extends ReactContextBaseJavaModule {
     mReactContext = reactContext;
   }
 
+  @NonNull
   @Override
   public String getName() {
-    return "Pedometer";
+    return MODULE_NAME;
   }
 
   @ReactMethod
   public void isSupported(Promise promise) {
-    if (mStepCounter == null)
-      mStepCounter = new StepCounter(mReactContext);
-    promise.resolve(mStepCounter.isSupported());
+    boolean supported = acquireStepCounter().isSupported();
+    promise.resolve(supported);
   }
 
   @ReactMethod
   public void start(int periodMs, Promise promise) {
-    if (mStepCounter == null)
-      mStepCounter = new StepCounter(mReactContext);
-    boolean supported = mStepCounter.isSupported();
+    StepCounter stepCounter = acquireStepCounter();
+    boolean supported = stepCounter.isSupported();
     if (supported)
-      mStepCounter.start(periodMs);
+      stepCounter.start(periodMs);
     promise.resolve(supported);
   }
 
   @ReactMethod
   public void stop() {
-    if (mStepCounter != null)
-      mStepCounter.stop();
+    StepCounter stepCounter = acquireStepCounter(false);
+    if (stepCounter != null)
+      stepCounter.stop();
   }
 
   @ReactMethod
   public void single(Promise promise) {
-    if (mStepCounter == null)
-      mStepCounter = new StepCounter(mReactContext);
-    boolean supported = mStepCounter.isSupported();
+    StepCounter stepCounter = acquireStepCounter();
+    boolean supported = stepCounter.isSupported();
     if (supported)
-      mStepCounter.single();
+      stepCounter.single();
     promise.resolve(supported);
   }
 
   @ReactMethod
   public void cancelSingle() {
-    if (mStepCounter != null)
-      mStepCounter.cancelSingle();
+    StepCounter stepCounter = acquireStepCounter(false);
+    if (stepCounter != null)
+      stepCounter.cancelSingle();
+  }
+
+  private StepCounter acquireStepCounter() {
+    return acquireStepCounter(true);
+  }
+
+  @Nullable
+  private StepCounter acquireStepCounter(boolean createIfNull) {
+    if (mStepCounter == null && createIfNull)
+      mStepCounter = new StepCounter(mReactContext);
+    return mStepCounter;
   }
 }
